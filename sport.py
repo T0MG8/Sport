2ba1f38f185441bc3ed83be3096627559fa4c4a9
T0MG8 <tom.goede@hva.nl>
2025-03-24 15:27:50 +0100

    Add files via upload

--- sport.py
+++ sport.py
@@ -9,7 +9,7 @@ excel_bestand = 'Data sporten.xlsx'
 if os.path.exists(excel_bestand):
     df = pd.read_excel(excel_bestand)
 else:
-    df = pd.DataFrame(columns=['Datum', 'Oefening', 'Hoevaak'])
+    df = pd.DataFrame(columns=['Datum', 'Oefening', 'Hoevaak', 'Gewicht'])
 
 # Functie om gegevens op te slaan
 def save_to_excel(data):
@@ -27,6 +27,7 @@ page = st.sidebar.radio("Kies een optie", ["Home", "Formulier"], index=["Home", 
 # Oefening- en herhalingsopties
 oefeningen = ['Dumbell Press', 'Zittend Roeien Cables', 'Incline Bench Press', 'Tricep Extencion']
 hoevaak_opties = ['3x 10', '3x 9', '3x 8', '3x 7', '3x 6']
+gewicht_opties = ['5kg', '10kg', '15kg', '20kg']  # Toegevoegde gewichten
 
 if page == "Formulier":
     st.title('Formulier om Excel-bestand bij te werken')
@@ -35,37 +36,39 @@ if page == "Formulier":
     vandaag = datetime.today().strftime('%Y-%m-%d')
     Datum = st.text_input('Datum', value=vandaag)
 
-    # Initialiseer lijst voor oefeningen
+    # Initialiseer lijst voor oefeningen, herhalingen en gewichten
     if 'oefeningen' not in st.session_state:
-        st.session_state.oefeningen = [{'oef': oefeningen[0], 'rep': hoevaak_opties[0]}]
+        st.session_state.oefeningen = [{'oef': oefeningen[0], 'rep': hoevaak_opties[0], 'gewicht': gewicht_opties[0]}]
 
     # Toon de dynamische invoervelden
     for i, oefening in enumerate(st.session_state.oefeningen):
-        col1, col2 = st.columns([2, 1])
+        col1, col2, col3 = st.columns([2, 1, 1])  # Voeg een extra kolom voor gewicht toe
         with col1:
             oefening['oef'] = st.selectbox(f'Oefening {i+1}', oefeningen, index=oefeningen.index(oefening['oef']), key=f'oef_{i}')
         with col2:
             oefening['rep'] = st.selectbox(f'Hoevaak {i+1}', hoevaak_opties, index=hoevaak_opties.index(oefening['rep']), key=f'rep_{i}')
+        with col3:  # Het nieuwe dropdownmenu voor gewicht
+            oefening['gewicht'] = st.selectbox(f'Gewicht {i+1}', gewicht_opties, index=gewicht_opties.index(oefening['gewicht']), key=f'gewicht_{i}')
 
     # Knoppen: Extra oefening & Opslaan
     col1, col2 = st.columns([3, 1])
     with col1:
         if st.button('➕ Extra oefening'):
-            st.session_state.oefeningen.append({'oef': oefeningen[0], 'rep': hoevaak_opties[0]})
+            st.session_state.oefeningen.append({'oef': oefeningen[0], 'rep': hoevaak_opties[0], 'gewicht': gewicht_opties[0]})
             st.rerun()
 
     with col2:
         if st.button('✅ Opslaan'):
-            if Datum and all(oef['oef'] and oef['rep'] for oef in st.session_state.oefeningen):
-                data = [{'Datum': Datum, 'Oefening': oef['oef'], 'Hoevaak': oef['rep']} for oef in st.session_state.oefeningen]
+            if Datum and all(oef['oef'] and oef['rep'] and oef['gewicht'] for oef in st.session_state.oefeningen):
+                data = [{'Datum': Datum, 'Oefening': oef['oef'], 'Hoevaak': oef['rep'], 'Gewicht': oef['gewicht']} for oef in st.session_state.oefeningen]
                 save_to_excel(data)
                 st.success('✅ Gegevens succesvol opgeslagen!')
 
                 # Wacht 3 seconden voordat de pagina verandert
-                time.sleep(3)
+                time.sleep(5)
 
                 # Reset en terug naar Home
-                st.session_state.oefeningen = [{'oef': oefeningen[0], 'rep': hoevaak_opties[0]}]
+                st.session_state.oefeningen = [{'oef': oefeningen[0], 'rep': hoevaak_opties[0], 'gewicht': gewicht_opties[0]}]
                 st.session_state.page = "Home"
                 st.rerun()
             else:
@@ -74,3 +77,4 @@ if page == "Formulier":
 else:
     st.title("Welkom op de Homepagina")
     st.write("Klik op de 'Formulier' optie in de sidebar om gegevens in te vullen.")
+
